ee8f7de2320d6eb3f7da378fd61c215cac33480b
yxni <yxni@mit.edu>
2024-05-23 08:54:24 -0700

    backend API to Attom or find property data in local database

--- django_api_accretion/django_api_accretion/database_visualization/models.py
+++ django_api_accretion/django_api_accretion/database_visualization/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Deed record database based on Attoms ID  
+class PropertyData(models.Model): 
+    propertyID = models.CharField(max_length=255, unique=True) 
+    data = models.JSONField() 
+    
+    def __str__(self): 
+        return self.propertyID 
--- django_api_accretion/django_api_accretion/database_visualization/views.py
+++ django_api_accretion/django_api_accretion/database_visualization/views.py
@@ -2,32 +2,45 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.views import APIView
 import requests
+from .models import PropertyData
+
+API_key_Attoms = "596d978f1497eafd93fd11d382c11525" 
 
 class DatabaseVisualizationView(APIView):
     def get(self, request):
-        # address1 = request.GET.get('address1', '')
-        # address2 = request.GET.get('address2', '')
         address1 = request.GET.get('address1','')
         address2 = request.GET.get('address2','')
-        googleMapID = request.GET.get('googlemapid','')
+        propertyID = request.GET.get('propertyid','')
         print("===addressInfo===", address1, "===address2===", address2)
-        print("===googlemapid===", googleMapID)
+        print("===propertyID===", propertyID)
 
-        # if not address1 or not address2:
-        #     return JsonResponse({'error': 'Missing address parameters'}, status=400)
+        if not address1 or not address2:
+            return JsonResponse({'error': 'Missing address parameters'}, status=400)
 
-        # Construct the API call to the third-party service
-        api_url = f'https://api.attom.com/property/v3/{address1}'
+        try: #retrieve property data from Accretion Database
+            propertyData = PropertyData.objects.get(propertyID = propertyID) 
+            print("===property found in local database===")
+            return JsonResponse(propertyData.data) 
+        except: #retrieve property data from Attom through an API call 
+            api_url = "https://api.gateway.attomdata.com/propertyapi/v1.0.0/saleshistory/basichistory" + f'?address1={address1}' + f'&address2={address2}'
 
-        headers = {
-            'apikey': 'your_attom_api_key',  # Replace with your actual API key
-        }
+            headers = {
+                'apikey': API_key_Attoms,  # Replace with your actual API key
+            }
 
-        try:
-            response = requests.get(api_url, headers=headers)
-            response.raise_for_status()  # Raise an exception for HTTP errors
-            data = response.json()
-        except requests.RequestException as e:
-            return JsonResponse({'error': str(e)}, status=500)
+            try:
+                response = requests.get(api_url, headers=headers)
+                response.raise_for_status()  # Raise an exception for HTTP errors
+                data = response.json()
+                try: # store the data into local database  
+                    PropertyData.objects.create(
+                        propertyID = data["status"]["attomId"], 
+                        data = data 
+                    )
+                except Exception as e: 
+                    print(e) 
+                    
+            except requests.RequestException as e:
+                return JsonResponse({'error': str(e)}, status=500)
 
-        return JsonResponse(data)
+            return JsonResponse(data)

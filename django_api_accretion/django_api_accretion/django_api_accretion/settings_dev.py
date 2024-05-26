03949406646130c9f87324f8efae6703e090f12e
yxni <yxni@mit.edu>
2024-05-26 13:44:10 -0700

    move development environment database from sqlite to postgre

--- django_api_accretion/django_api_accretion/django_api_accretion/settings_dev.py
+++ django_api_accretion/django_api_accretion/django_api_accretion/settings_dev.py
@@ -13,9 +13,22 @@ DEBUG = os.getenv('ENVIRONMENT') == 'development'
 
 ALLOWED_HOSTS = []
 
+# SQL lite database configuration 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+# PostGres database configuration 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME", "accretiondatabase"),
+        'USER': os.getenv("DB_USER", os.getenv("DB_USER")),
+        'PASSWORD': os.getenv("DB_PASSWORD", os.getenv("DB_PASSWORD")),
+        'HOST': os.getenv("DB_HOST", "localhost"),
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
 }
--- django_api_accretion/django_api_accretion/django_api_accretion/settings_prod.py
+++ django_api_accretion/django_api_accretion/django_api_accretion/settings_prod.py
@@ -16,7 +16,27 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_URL = '/static/'
+
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+# Static files (CSS, JavaScript, images)
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
+
+# Media files
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Security settings
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True

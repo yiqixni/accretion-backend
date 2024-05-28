de52bf21141c2ad68b1521abbb214ac5e2b8ea5a
yxni <yxni@mit.edu>
2024-05-27 23:50:09 -0700

    run DRF on Docker container worked v0

--- django_api_accretion/django_api_accretion/django_api_accretion/settings_base.py
+++ django_api_accretion/django_api_accretion/django_api_accretion/settings_base.py
@@ -100,10 +100,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -157,9 +154,7 @@ CORS_ALLOWED_HEADERS = [
     
 ]
 
-# UPLOADED FILES CONFIGURATION
-MEDIA_URL = "/media/" 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media") 
+
 
 # Configure Djoser
 DJOSER = {
--- django_api_accretion/django_api_accretion/django_api_accretion/settings_dev.py
+++ django_api_accretion/django_api_accretion/django_api_accretion/settings_dev.py
@@ -13,7 +13,18 @@ DEBUG = os.getenv('ENVIRONMENT') == 'development'
 
 ALLOWED_HOSTS = []
 
-# SQL lite database configuration 
+#### STATIC/MEDIA FILE CONFIGURATION ####
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = "static/"
+
+# UPLOADED FILES CONFIGURATION
+MEDIA_URL = "/media/" 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media") 
+
+
+# # SQL lite database configuration # Deprecated # 
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.sqlite3",
@@ -24,11 +35,15 @@ ALLOWED_HOSTS = []
 # PostGres database configuration 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME", "accretiondatabase"),
+        'ENGINE': 'django.db.backends.postgresql',        
+        'NAME': os.getenv("DB_NAME", os.getenv("DB_NAME")),
         'USER': os.getenv("DB_USER", os.getenv("DB_USER")),
-        'PASSWORD': os.getenv("DB_PASSWORD", os.getenv("DB_PASSWORD")),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "5432"),
+        'PASSWORD': os.getenv("DB_PASSWORD", os.getenv("DB_PASSWORD")),        
+        'HOST': os.getenv("DB_HOST", os.getenv("DB_HOST")), # for Docker container        
+        'PORT': os.getenv("DB_PORT", os.getenv("DB_PORT")),
+        # 'NAME': os.getenv("DB_NAME", "accretiondatabase"),
+        # 'HOST': "localhost", # for running backend without Docker         
+        # 'HOST': os.getenv("DB_HOST", "db"),
+        # 'PORT': os.getenv("DB_PORT", "5432"),        
     }
 }

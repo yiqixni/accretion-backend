6dc56cd2bb3a02a091f2b2aa34bc63df3f0e09a2
yxni <yxni@mit.edu>
2024-06-03 14:49:39 -0700

    brokend backend: GET request from frontend fails due to CORS policy

--- django_api_accretion/django_api_accretion/django_api_accretion/settings_dev.py
+++ django_api_accretion/django_api_accretion/django_api_accretion/settings_dev.py
@@ -12,10 +12,11 @@ ALLOWED_HOSTS = []
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-
 # UPLOADED FILES CONFIGURATION
+STATIC_URL = "/static/"
 MEDIA_URL = "/media/" 
+
+## media root for local dev
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media") 
 
 ## static root for Docker 

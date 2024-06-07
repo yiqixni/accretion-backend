958d21ca81c2245601759961886035de6b483b9f
yxni <yxni@mit.edu>
2024-06-07 01:10:34 -0700

    rewrite the Dockerfile.certbot to ensure certificates are created, rewrite the nginx.conf.prod to point to the correct locations for private and public keys

--- django_api_accretion/certbot/entrypoin.sh
+++ django_api_accretion/certbot/entrypoin.sh
@@ -3,9 +3,21 @@
 # Check if we have existing certificates
 if [ ! -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
   echo "Certificates not found, creating new ones..."
-  certbot certonly --webroot -w /data/letsencrypt -d $DOMAIN --email $CERTBOT_EMAIL --agree-tos --no-eff-email
+#   certbot certonly --webroot -w /data/letsencrypt -d $DOMAIN --email $CERTBOT_EMAIL --agree-tos --no-eff-email
+  certbot certonly --nginx -d $DOMAIN --email $CERTBOT_EMAIL --agree-tos --no-eff-email
 else
   echo "Certificates found, skipping generation."
+fi
+
+# Check if placeholder files exist and copy them if necessary
+if [ ! -f /etc/letsencrypt/options-ssl-nginx.conf ]; then
+  echo "Copying options-ssl-nginx.conf.placeholder to /etc/letsencrypt/options-ssl-nginx.conf"
+  cp /etc/letsencrypt/options-ssl-nginx.conf.placeholder /etc/letsencrypt/options-ssl-nginx.conf
+fi
+
+if [ ! -f /etc/letsencrypt/ssl-dhparams.pem ]; then
+  echo "Copying ssl-dhparams.pem.placeholder to /etc/letsencrypt/ssl-dhparams.pem"
+  cp /etc/letsencrypt/ssl-dhparams.pem.placeholder /etc/letsencrypt/ssl-dhparams.pem
 fi
 
 # Start the certbot renew process in the background

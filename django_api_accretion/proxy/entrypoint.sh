3cab7b76179862869290b3f49cedbd15876bd380
yxni <yxni@mit.edu>
2024-06-07 10:13:29 -0700

    grant error log permission in entrypoint.sh

--- django_api_accretion/proxy/entrypoint.sh
+++ django_api_accretion/proxy/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "make directory and grant permission for /var/log/nginx..."
+
+# Create log directory and set permissions
+mkdir -p /var/log/nginx
+chown -R www-data:www-data /var/log/nginx
+
 echo "start nginx service entrypoint.sh..."
 
 nginx -g 'daemon off;' 

d05c6f1eb509833236cb93634bd70d6ca21b63d1
yxni <yxni@mit.edu>
2024-06-07 07:46:45 -0700

    disable certbot service entrypoint.sh for debug

--- django_api_accretion/certbot/entrypoint.sh
+++ django_api_accretion/certbot/entrypoint.sh
@@ -5,29 +5,29 @@ nginx -v 2>&1
 echo "Checking Certbot version..."
 certbot --version 2>&1
 
-# Check if we have existing certificates
-if [ ! -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
-  echo "Certificates not found, creating new ones at /etc/letsencrypt/live/$DOMAIN/fullchain.pem with email $CERTBOT_EMAIL ..."
-  certbot certonly --nginx -d $DOMAIN --email $CERTBOT_EMAIL --agree-tos --no-eff-email  
-  echo "End of certbot creation..."
-else
-  echo "Certificates found, skipping generation."
-fi
+# # Check if we have existing certificates
+# if [ ! -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
+#   echo "Certificates not found, creating new ones at /etc/letsencrypt/live/$DOMAIN/fullchain.pem with email $CERTBOT_EMAIL ..."
+#   certbot certonly --nginx -d $DOMAIN --email $CERTBOT_EMAIL --agree-tos --no-eff-email  
+#   echo "End of certbot creation..."
+# else
+#   echo "Certificates found, skipping generation."
+# fi
 
-# Check if options-ssl-nginx.conf is in place
-if [ ! -f /etc/letsencrypt/options-ssl-nginx.conf ]; then
-  echo "Missing /etc/letsencrypt/options-ssl-nginx.conf, creating it..."
-  certbot --nginx -d $DOMAIN --agree-tos --email $CERTBOT_EMAIL --deploy-hook "cp /etc/letsencrypt/options-ssl-nginx.conf /etc/letsencrypt/options-ssl-nginx.conf"
-fi
+# # Check if options-ssl-nginx.conf is in place
+# if [ ! -f /etc/letsencrypt/options-ssl-nginx.conf ]; then
+#   echo "Missing /etc/letsencrypt/options-ssl-nginx.conf, creating it..."
+#   certbot --nginx -d $DOMAIN --agree-tos --email $CERTBOT_EMAIL --deploy-hook "cp /etc/letsencrypt/options-ssl-nginx.conf /etc/letsencrypt/options-ssl-nginx.conf"
+# fi
 
-# Check if ssl-dhparams.pem is in place
-if [ ! -f /etc/letsencrypt/ssl-dhparams.pem ]; then
-  echo "Missing /etc/letsencrypt/ssl-dhparams.pem, creating it..."
-  openssl dhparam -out /etc/letsencrypt/ssl-dhparams.pem 2048
-fi
+# # Check if ssl-dhparams.pem is in place
+# if [ ! -f /etc/letsencrypt/ssl-dhparams.pem ]; then
+#   echo "Missing /etc/letsencrypt/ssl-dhparams.pem, creating it..."
+#   openssl dhparam -out /etc/letsencrypt/ssl-dhparams.pem 2048
+# fi
 
-# Start the certbot renew process in the background
-while :; do
-  certbot renew
-  sleep 12h # Sleep for 12 hours before checking again
-done &
+# # Start the certbot renew process in the background
+# while :; do
+#   certbot renew
+#   sleep 12h # Sleep for 12 hours before checking again
+# done &

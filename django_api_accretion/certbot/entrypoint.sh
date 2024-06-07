16a859ff9cb188195e6befd7aedcff3e300b683c
yxni <yxni@mit.edu>
2024-06-07 01:32:56 -0700

    edit dockerfile.certbot and entrypoint.sh for certbot to ensure certificates are created

--- django_api_accretion/certbot/entrypoint.sh
+++ django_api_accretion/certbot/entrypoint.sh
@@ -3,21 +3,21 @@
 # Check if we have existing certificates
 if [ ! -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
   echo "Certificates not found, creating new ones..."
-#   certbot certonly --webroot -w /data/letsencrypt -d $DOMAIN --email $CERTBOT_EMAIL --agree-tos --no-eff-email
   certbot certonly --nginx -d $DOMAIN --email $CERTBOT_EMAIL --agree-tos --no-eff-email
 else
   echo "Certificates found, skipping generation."
 fi
 
-# Check if placeholder files exist and copy them if necessary
+# Check if options-ssl-nginx.conf is in place
 if [ ! -f /etc/letsencrypt/options-ssl-nginx.conf ]; then
-  echo "Copying options-ssl-nginx.conf.placeholder to /etc/letsencrypt/options-ssl-nginx.conf"
-  cp /etc/letsencrypt/options-ssl-nginx.conf.placeholder /etc/letsencrypt/options-ssl-nginx.conf
+  echo "Missing /etc/letsencrypt/options-ssl-nginx.conf, creating it..."
+  certbot --nginx -d $DOMAIN --agree-tos --email $CERTBOT_EMAIL --deploy-hook "cp /etc/letsencrypt/options-ssl-nginx.conf /etc/letsencrypt/options-ssl-nginx.conf"
 fi
 
+# Check if ssl-dhparams.pem is in place
 if [ ! -f /etc/letsencrypt/ssl-dhparams.pem ]; then
-  echo "Copying ssl-dhparams.pem.placeholder to /etc/letsencrypt/ssl-dhparams.pem"
-  cp /etc/letsencrypt/ssl-dhparams.pem.placeholder /etc/letsencrypt/ssl-dhparams.pem
+  echo "Missing /etc/letsencrypt/ssl-dhparams.pem, creating it..."
+  openssl dhparam -out /etc/letsencrypt/ssl-dhparams.pem 2048
 fi
 
 # Start the certbot renew process in the background
